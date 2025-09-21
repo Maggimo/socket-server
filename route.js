7ebaee00786702e1aa89587de2e357d584115623
Maggimo <basirovmb1988@gmail.com>
2025-09-21 13:51:45 +0300

    added cors headers

--- route.js
+++ route.js
@@ -2,6 +2,10 @@ const express = require('express')
 const router = express.Router()
 
 router.get('/', (req, res) => {
+	res.setHeader('Access-Control-Allow-Origin', '*')
+	res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE')
+	res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type')
+
 	res.send('request send')
 })
 
